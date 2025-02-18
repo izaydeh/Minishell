f77f92c97b5597082c069a2c52d8fd06ed1d2e5e
Shoaib Al-Kawaldeh <shoaibmohkh9@gmail.com>
2025-02-18 14:52:32 +0300

    edit the echo to handle this case echo -nnennn shoaib

--- src/builtin/ft_echo.c
+++ src/builtin/ft_echo.c
@@ -1,27 +1,57 @@
 #include "minishell.h"
 
+
 void ft_echo(t_shell *string, int i)
 {
     int j = 0;
-    if (string->command[i][1] == NULL && ft_strcmp(string->command[i][0], "echo") == 0)
-        write(1, "\n", 1);
-    else if (ft_strcmp(string->command[i][1], "-n") == 0)
+    int x = 1;
+    int f = 0;
+    if (!string->command[i][1])
     {
-        while (string->command[i][2][j])
+        write(1, "\n", 1);
+        return;
+    }
+    if (string->command[i][1][0] == '-' && string->command[i][1][1] == 'n')
+    {
+        while (string->command[i][1][x])
         {
-            write(1, &string->command[i][2][j], 1);
-            j++;
+            if (string->command[i][1][x] != 'n' && string->command[i][1][x] != 'e') 
+            {
+                f = 1;
+                break;
+            }
+            x++;
+        }
+        if (f == 0)
+        {
+            x = 2;
+            while (string->command[i][x])
+            {
+                j = 0;
+                while (string->command[i][x][j])
+                {
+                    write(1, &string->command[i][x][j], 1);
+                    j++;
+                }
+                if (string->command[i][x + 1])
+                    write(1, " ", 1);
+                x++;
+            }
+            return;
         }
     }
-    else if (ft_strcmp(string->command[i][1], "-n") != 0)
+    x = 1;
+    while (string->command[i][x])
     {
-        while (string->command[i][1][j])
+        j = 0;
+        while (string->command[i][x][j])
         {
-            write(1, &string->command[i][1][j], 1);
+            write(1, &string->command[i][x][j], 1);
             j++;
         }
-        write(1, "\n", 1);
+        if (string->command[i][x + 1])
+            write(1, " ", 1);
+        x++;
     }
-    else
-        perror("there is error in ehco");
+    write(1, "\n", 1);
 }
